141aaf6f8e6548f9fc17998e113cadb8df7458c9
sofiia koval <kovalsofiia1@gmail.com>
2025-02-16 22:45:11 +0200

    task 5

---
+++ src/main/java/example/com/task5/Counter.java
@@ -0,0 +1,64 @@
+package example.com.task5;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+class Counter {
+    private int count = 0;
+    private final Object lock = new Object();
+    private final Lock reentrantLock = new ReentrantLock();
+
+    // 1. Несинхронізовані методи (демонстрація гонки потоків)
+    public void increment() {
+        count++;
+    }
+
+    public void decrement() {
+        count--;
+    }
+
+    // 2. Синхронізовані методи
+    public synchronized void synchronizedIncrement() {
+        count++;
+    }
+
+    public synchronized void synchronizedDecrement() {
+        count--;
+    }
+
+    // 3. Синхронізований блок
+    public void blockSynchronizedIncrement() {
+        synchronized (lock) {
+            count++;
+        }
+    }
+
+    public void blockSynchronizedDecrement() {
+        synchronized (lock) {
+            count--;
+        }
+    }
+
+    // 4. Використання ReentrantLock
+    public void lockIncrement() {
+        reentrantLock.lock();
+        try {
+            count++;
+        } finally {
+            reentrantLock.unlock();
+        }
+    }
+
+    public void lockDecrement() {
+        reentrantLock.lock();
+        try {
+            count--;
+        } finally {
+            reentrantLock.unlock();
+        }
+    }
+
+    public int getCount() {
+        return count;
+    }
+}
---
+++ src/main/java/example/com/task5/CounterTest.java
@@ -0,0 +1,71 @@
+package example.com.task5;
+
+
+public class CounterTest {
+    private static final int ITERATIONS = 100000;
+
+    public static void main(String[] args) throws InterruptedException {
+        CounterTest test = new CounterTest();
+
+        System.out.println("=== Not sync ===");
+        test.testCounterWithoutSync();
+
+        System.out.println("=== Sync method ===");
+        test.testCounterWithSynchronizedMethods();
+
+        System.out.println("=== Sync block ===");
+        test.testCounterWithSynchronizedBlock();
+
+        System.out.println("=== ReentrantLock ===");
+        test.testCounterWithLock();
+    }
+
+    private void testCounterWithoutSync() throws InterruptedException {
+        Counter counter = new Counter();
+        runTest(counter, false, false, false);
+        System.out.println("Result: " + counter.getCount());
+    }
+
+    private void testCounterWithSynchronizedMethods() throws InterruptedException {
+        Counter counter = new Counter();
+        runTest(counter, true, false, false);
+        System.out.println("Result: " + counter.getCount());
+    }
+
+    private void testCounterWithSynchronizedBlock() throws InterruptedException {
+        Counter counter = new Counter();
+        runTest(counter, false, true, false);
+        System.out.println("Result: " + counter.getCount());
+    }
+
+    private void testCounterWithLock() throws InterruptedException {
+        Counter counter = new Counter();
+        runTest(counter, false, false, true);
+        System.out.println("Result: " + counter.getCount());
+    }
+
+    private void runTest(Counter counter, boolean syncMethod, boolean syncBlock, boolean useLock) throws InterruptedException {
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i < ITERATIONS; i++) {
+                if (syncMethod) counter.synchronizedIncrement();
+                else if (syncBlock) counter.blockSynchronizedIncrement();
+                else if (useLock) counter.lockIncrement();
+                else counter.increment();
+            }
+        });
+
+        Thread t2 = new Thread(() -> {
+            for (int i = 0; i < ITERATIONS; i++) {
+                if (syncMethod) counter.synchronizedDecrement();
+                else if (syncBlock) counter.blockSynchronizedDecrement();
+                else if (useLock) counter.lockDecrement();
+                else counter.decrement();
+            }
+        });
+
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+    }
+}
