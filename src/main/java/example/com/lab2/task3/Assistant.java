83d4f04d9983abc8565f49bbed89df586ebb017b
sofiia koval <kovalsofiia1@gmail.com>
2025-02-23 14:01:03 +0200

    lab2: task 3 modify

--- src/main/java/example/com/lab2/task3/Assistant.java
+++ src/main/java/example/com/lab2/task3/Assistant.java
@@ -2,34 +2,6 @@ package example.com.lab2.task3;
 
 import java.util.Random;
 
-//class Assistant implements Runnable {
-//    private final GradeBook gradeBook;
-//    private final String name;
-//    private final int assignedGroup;
-//    private final Random random = new Random();
-//
-//    public Assistant(GradeBook gradeBook, String name, int assignedGroup) {
-//        this.gradeBook = gradeBook;
-//        this.name = name;
-//        this.assignedGroup = assignedGroup;
-//    }
-//
-//    @Override
-//    public void run() {
-//        for (int week = 1; week <= 3; week++) {
-//            for (int student = 0; student < 5; student++) {
-//                int grade = random.nextInt(81) + 20;
-//                gradeBook.addGrade(assignedGroup, student, grade, name);
-//            }
-//            try {
-//                Thread.sleep(random.nextInt(100));
-//            } catch (InterruptedException e) {
-//                Thread.currentThread().interrupt();
-//            }
-//        }
-//    }
-//}
-
 class Assistant implements Runnable {
     private final GradeBook gradeBook;
     private final String name;
--- src/main/java/example/com/lab2/task3/GradeBook.java
+++ src/main/java/example/com/lab2/task3/GradeBook.java
@@ -6,46 +6,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.locks.ReentrantLock;
 
-//
-//class GradeBook {
-//    private final List<List<List<Integer>>> journal;
-//    private final ReentrantLock lock = new ReentrantLock();
-//    private final int numGroups;
-//    private final int studentsPerGroup;
-//
-//    public GradeBook(int numGroups, int studentsPerGroup) {
-//        this.numGroups = numGroups;
-//        this.studentsPerGroup = studentsPerGroup;
-//        journal = new ArrayList<>();
-//        for (int i = 0; i < numGroups; i++) {
-//            List<List<Integer>> group = new ArrayList<>();
-//            for (int j = 0; j < studentsPerGroup; j++) {
-//                group.add(new ArrayList<>());
-//            }
-//            journal.add(group);
-//        }
-//    }
-//
-//    public void addGrade(int groupIndex, int studentIndex, int grade, String teacher) {
-//        lock.lock();
-//        try {
-//            journal.get(groupIndex).get(studentIndex).add(grade);
-//            System.out.printf("%s assigned %d points to Student %d in Group %d%n", teacher, grade, studentIndex + 1, groupIndex + 1);
-//        } finally {
-//            lock.unlock();
-//        }
-//    }
-//
-//    public void printJournal() {
-//        System.out.println("\nElectronic Journal:");
-//        for (int i = 0; i < numGroups; i++) {
-//            System.out.println("Group " + (char) ('A' + i) + ":");
-//            for (int j = 0; j < studentsPerGroup; j++) {
-//                System.out.printf("  Student %d: %s%n", j + 1, journal.get(i).get(j));
-//            }
-//        }
-//    }
-//}
 
 class GradeBook {
     private final List<List<List<Integer>>> journal;
