329455d3f2a4a869c64dc64778e4be55db60a5c1
sofiia koval <kovalsofiia1@gmail.com>
2025-03-10 21:42:14 +0200

    lab 3: pass blocks in fox

--- src/main/java/example/com/lab3/FoxMatrixMultiplication.java
+++ src/main/java/example/com/lab3/FoxMatrixMultiplication.java
@@ -9,11 +9,20 @@ class FoxMatrixMultiplication {
         int blockSize = Math.max(1, matrixSize / (int) Math.sqrt(threadCount));
         ExecutorService executor = Executors.newFixedThreadPool(threadCount);
         ResultMatrix resultMatrix = new ResultMatrix(matrixA.getRows(), matrixA.getCols());
+
         for (int rowBlock = 0; rowBlock < matrixSize; rowBlock += blockSize) {
             for (int colBlock = 0; colBlock < matrixSize; colBlock += blockSize) {
-                final int startRow = rowBlock;
-                final int startCol = colBlock;
-                executor.execute(() -> multiplyBlock(matrixA, matrixB, resultMatrix, startRow, startCol, blockSize));
+                for (int k = 0; k < matrixA.getCols(); k += blockSize) {
+                    final int[][] blockA = matrixA.getBlock(rowBlock / blockSize, k / blockSize, blockSize);
+                    final int[][] blockB = matrixB.getBlock(k / blockSize, colBlock / blockSize, blockSize);
+                    final int blockRow = rowBlock / blockSize;
+                    final int blockCol = colBlock / blockSize;
+
+                    executor.execute(() -> {
+                        int[][] blockResult = multiplyBlock(blockA, blockB, blockSize);
+                        resultMatrix.addBlock(blockResult, blockRow, blockCol);
+                    });
+                }
             }
         }
 
@@ -22,22 +31,18 @@ class FoxMatrixMultiplication {
         return resultMatrix;
     }
 
-    private static void multiplyBlock(Matrix matrixA, Matrix matrixB, Matrix resultMatrix, int startRow, int startCol, int blockSize) {
-        for (int k = 0; k < matrixA.getCols(); k += blockSize) {
-            int[][] blockA = matrixA.getBlock(startRow / blockSize, k / blockSize, blockSize);
-            int[][] blockB = matrixB.getBlock(k / blockSize, startCol / blockSize, blockSize);
-            int[][] blockResult = new int[blockSize][blockSize];
+    private static int[][] multiplyBlock(int[][] blockA, int[][] blockB, int blockSize) {
+        int[][] blockResult = new int[blockSize][blockSize];
 
-            for (int i = 0; i < blockSize && startRow + i < matrixA.getRows(); i++) {
-                for (int j = 0; j < blockSize && startCol + j < matrixA.getCols(); j++) {
-                    int sum = 0;
-                    for (int x = 0; x < blockSize && k + x < matrixA.getCols(); x++) {
-                        sum += blockA[i][x] * blockB[x][j];
-                    }
-                    blockResult[i][j] = sum;
+        for (int i = 0; i < blockSize; i++) {
+            for (int j = 0; j < blockSize; j++) {
+                int sum = 0;
+                for (int x = 0; x < blockSize; x++) {
+                    sum += blockA[i][x] * blockB[x][j];
                 }
+                blockResult[i][j] = sum;
             }
-            resultMatrix.addBlock(blockResult, startRow / blockSize, startCol / blockSize);
         }
+        return blockResult;
     }
 }
